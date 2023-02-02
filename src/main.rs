c1f5d39a7bf3f158b4a96770b7cd3021f5e05ebc
gneb000 <gneb000@disroot.org>
2023-02-02 23:58:51 +0100

    Implement get_col_lens()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/*struct Book {
+    title: String,
+    author: String,
+    series: String,
+    series_index: usize,
+    pages: u32,
+    path: String,
+}*/
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "caldb.txt";
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut lines = reader.lines();
+
+    let header_line = lines.next().unwrap().unwrap();
+    let col_lens = get_col_lens(header_line);
+}
+
+fn get_col_lens(line: String) -> Vec<i32> {
+    let mut col_lens = Vec::new();
+    let mut count = -1;
+    let mut space_start = false;
+    for c in line.chars() {
+        count += 1;
+        if c == ' ' {
+            space_start = true;
+        } else if space_start && c != ' ' {
+            space_start = false;
+            col_lens.push(count);
+            count = 0;
+        }
+    }
+    col_lens
 }
